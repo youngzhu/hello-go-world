0d404802935f504c5990953b88acc7ead2fe515a
Young-Win <young@win.com>
2021-09-14 22:09:01 +0800

    Update main.go
    测试了日期函数

--- widget/edslogger/src/main/main.go
+++ widget/edslogger/src/main/main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"log"
-	"os"
-	"secret"
+	"math/rand"
+	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
 func main() {
-	log.Println(os.Getwd()) // 可以查看当前的工作目录
-	secret, err := secret.RetrieveSecret()
-	if err != nil {
-		log.Fatal(err)
+	// log.Println(os.Getwd()) // 可以查看当前的工作目录
+	// secret, err := secret.RetrieveSecret()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(secret)
+
+	// 获取当前时间
+	t := time.Now()
+	log.Println(t)
+
+	for i := 0; i < 5; i++ {
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+		t = t.Add(time.Hour * 24)
+		// log.Println(fmt.Sprintf("%d-%d-%d", t.Year(), t.Month(), t.Day()))
+		// "2006-01-02 15:04:05"
+		// 这算小彩蛋吗？还必须这个时间才行。。。
+		log.Println(t.Format("2006-01-02"))
 	}
-	log.Println(secret)
+
 }
