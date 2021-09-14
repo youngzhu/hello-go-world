4a288d858e2f799eea538dc9db03d50a0fd3ede9
Young-Win <young@win.com>
2021-09-14 09:21:51 +0800

    将http部分的功能重构至http.go

--- widget/edslogger/src/eds_logger.go
+++ widget/edslogger/src/eds_logger.go
@@ -2,30 +2,20 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
 
+	myhttp "http"
 	"secret"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const URL_HOME = "http://eds.newtouch.cn"
-const URL_LOGIN = "http://eds.newtouch.cn/eds3/DefaultLogin.aspx?lan=zh-cn"
-const HOST = "eds.newtouch.cn"
-const USET_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
-const ACCEPT_LANGUAGE = "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7"
-const ACCEPT_ENCODING = "gzip, deflate"
-
-var postProperties = make(map[string]string)
-var getProperties = make(map[string]string)
-
 var secretInfo *secret.Secret
 var err error
+var cookie string
 
 func init() {
 	secretInfo, err = secret.RetrieveSecret()
@@ -33,83 +23,19 @@ func init() {
 		log.Fatal(err)
 	}
 
-	// post
-	postProperties["Host"] = HOST
-	postProperties["Content-Length"] = "6955"
-	postProperties["Cache-Control"] = "max-age=0"
-	postProperties["Origin"] = URL_HOME
-	postProperties["Upgrade-Insecure-Requests"] = "1"
-	postProperties["Content-Type"] = "application/x-www-form-urlencoded"
-	postProperties["User-Agent"] = "Mozilla/5.0"
-	postProperties["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"
-	postProperties["Accept-Encoding"] = ACCEPT_ENCODING
-	postProperties["Accept-Language"] = ACCEPT_LANGUAGE
-	postProperties["Cookie"] = secretInfo.Cookie
-	postProperties["connection"] = "Keep-Alive"
-	postProperties["accept"] = "*/*"
-	postProperties["user-agent"] = "Mozilla/5.0"
-
-	// get
-	getProperties["Upgrade-Insecure-Requests"] = "1"
-	getProperties["User-Agent"] = USET_AGENT
-	getProperties["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
-	getProperties["Accept-Encoding"] = ACCEPT_ENCODING
-	getProperties["Accept-Language"] = ACCEPT_LANGUAGE
-	getProperties["Cookie"] = secretInfo.Cookie
-	getProperties["Host"] = HOST
-}
-
-// http请求，返回 string
-func doRequest(url string, method string, body io.Reader) string {
-	log.Println(url)
-	request, err := http.NewRequest(method, url, body)
-	if err != nil {
-		panic(err)
-	}
-	request.Header.Set("Referer", url)
-
-	var headerProps map[string]string
-	if method == http.MethodPost {
-		headerProps = postProperties
-	} else {
-		headerProps = getProperties
-	}
-	for key, value := range headerProps {
-		request.Header.Set(key, value)
-	}
-
-	resp, err := http.DefaultClient.Do(request)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
-	// log.Println(resp.StatusCode)
-
-	if resp.StatusCode != http.StatusOK {
-		// io.Copy(os.Stdout, resp.Body)
-		log.Panicln(resp.StatusCode)
-	}
-
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	return string(respBody)
+	cookie = secretInfo.Cookie
 }
 
 func login() error {
 	// 检验网站是否正常
-	resp, err := http.Head(URL_HOME) // 只请求网站的 http header信息
+	resp, err := http.Head(myhttp.URL_HOME) // 只请求网站的 http header信息
 	if err != nil {
 		return err
 	}
 
 	defer resp.Body.Close()
 
+	loginUrl := "http://eds.newtouch.cn/eds3/DefaultLogin.aspx?lan=zh-cn"
 	// 登录
 	// data := `{"UserId":"###", "UserPsd":"***"}`
 	// data := "UserId=###&UserPsd=***"
@@ -120,31 +46,11 @@ func login() error {
 	params := url.Values{}
 	params.Add("UserId", secretInfo.UserId)
 	params.Add("UserPsd", secretInfo.UserPsd)
-	var request *http.Request
+	// var request *http.Request
 	// request, err = http.NewRequest(http.MethodPost, URL_LOGIN, strings.NewReader(data))
-	request, err = http.NewRequest(http.MethodPost, URL_LOGIN, strings.NewReader(params.Encode()))
-	if err != nil {
-		return err
-	}
-	request.Header.Set("Referer", URL_LOGIN)
-	for key, value := range postProperties {
-		request.Header.Set(key, value)
-	}
+	// request, err = http.NewRequest(http.MethodPost, loginUrl, strings.NewReader(params.Encode()))
 
-	resp, err = http.DefaultClient.Do(request)
-
-	if err != nil {
-		return nil
-	}
-
-	log.Println(resp.StatusCode)
-
-	if resp.StatusCode != http.StatusOK {
-		panic(resp.StatusCode)
-	}
-
-	// 查看一下
-	// io.Copy(os.Stdout, resp.Body)
+	myhttp.DoRequest(loginUrl, http.MethodPost, cookie, strings.NewReader(params.Encode()))
 
 	return nil
 }
@@ -160,8 +66,8 @@ func workLog(logDate string) {
 func getHiddenParams(url string) map[string]string {
 	result := make(map[string]string)
 
-	respHtml := doRequest(url, http.MethodGet, nil)
-	fmt.Println(respHtml)
+	respHtml := myhttp.DoRequest(url, http.MethodGet, cookie, nil)
+	// fmt.Println(respHtml)
 
 	keys := []string{"__EVENTVALIDATION", "__VIEWSTATE"}
 
---
+++ widget/edslogger/src/http/http.go
@@ -0,0 +1,89 @@
+package http
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const URL_HOME = "http://eds.newtouch.cn"
+const HOST = "eds.newtouch.cn"
+const USET_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
+const ACCEPT_LANGUAGE = "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7"
+const ACCEPT_ENCODING = "gzip, deflate"
+
+var postProperties = make(map[string]string)
+var getProperties = make(map[string]string)
+
+func init() {
+	// post
+	postProperties["Host"] = HOST
+	postProperties["Content-Length"] = "6955"
+	postProperties["Cache-Control"] = "max-age=0"
+	postProperties["Origin"] = URL_HOME
+	postProperties["Upgrade-Insecure-Requests"] = "1"
+	postProperties["Content-Type"] = "application/x-www-form-urlencoded"
+	postProperties["User-Agent"] = "Mozilla/5.0"
+	postProperties["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"
+	postProperties["Accept-Encoding"] = ACCEPT_ENCODING
+	postProperties["Accept-Language"] = ACCEPT_LANGUAGE
+	postProperties["connection"] = "Keep-Alive"
+	postProperties["accept"] = "*/*"
+	postProperties["user-agent"] = "Mozilla/5.0"
+
+	// get
+	getProperties["Upgrade-Insecure-Requests"] = "1"
+	getProperties["User-Agent"] = USET_AGENT
+	getProperties["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+	getProperties["Accept-Encoding"] = ACCEPT_ENCODING
+	getProperties["Accept-Language"] = ACCEPT_LANGUAGE
+	getProperties["Host"] = HOST
+}
+
+// http请求，返回 string
+func DoRequest(url, method, cookie string, body io.Reader) string {
+	log.Println(url)
+	request, err := http.NewRequest(method, url, body)
+	if err != nil {
+		panic(err)
+	}
+	request.Header.Set("Referer", url)
+
+	var headerProps map[string]string
+	if method == http.MethodPost {
+		headerProps = postProperties
+	} else {
+		headerProps = getProperties
+	}
+	for key, value := range headerProps {
+		request.Header.Set(key, value)
+	}
+
+	request.Header.Set("Cookie", cookie)
+
+	resp, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	// 查看一下
+	// io.Copy(os.Stdout, resp.Body)
+
+	// log.Println(resp.StatusCode)
+
+	if resp.StatusCode != http.StatusOK {
+		// io.Copy(os.Stdout, resp.Body)
+		log.Panicln(resp.StatusCode)
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(respBody)
+}
