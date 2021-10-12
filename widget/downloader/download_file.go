655aa7a2b1eb11f19a0fed3d9019f0b77ba41ba7
Young-Win <young@win.com>
2021-10-12 11:39:44 +0800

    可以下载了

--- widget/downloader/download_file.go
+++ widget/downloader/download_file.go
@@ -1,9 +1,29 @@
 package main
 
+import (
+	"net/http"
+	"io/ioutil"
+)
+
 var url = "https://algs4.cs.princeton.edu/21elementary/words3.txt"
 
 // 从指定url下载文件
 // 如果文件大于1000行，则压缩
 func main() {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
 
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("out/words3.txt", body, 0777)
+	if err != nil {
+		panic(err)
+	}
 }
