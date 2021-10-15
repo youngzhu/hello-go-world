d5481fb5b8cc0d9582ac2a55f520180db46140f7
Young-Win <young@win.com>
2021-10-15 09:25:37 +0800

    Create sort_compare.go

---
+++ widget/algs4/sort_compare.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+var newAlg string = "Quick"
+var otherAlgs []string
+
+func init() {
+	otherAlgs = append(otherAlgs, "Selection")
+	otherAlgs = append(otherAlgs, "Insertion")
+	otherAlgs = append(otherAlgs, "Shell")
+	otherAlgs = append(otherAlgs, "Merge")
+	// append(otherAlgs, "")
+	// append(otherAlgs, "")
+	// append(otherAlgs, "")
+	// append(otherAlgs, "")
+	// append(otherAlgs, "")
+
+
+	otherAlgs = append(otherAlgs, "Buildin")
+}
+
+func main() {
+	for _, v := range otherAlgs {
+		fmt.Println("//", newAlg, "vs", v)
+		fmt.Printf("// cmd: go run sort_compare.go -a1 %s -a2 %s -n 1000 -t 100 -s\n", newAlg, v)
+		fmt.Println("// got: ")
+		fmt.Printf("// cmd: go run sort_compare.go -a1 %s -a2 %s -n 1000 -t 100\n", newAlg, v)
+		fmt.Println("// got: ")
+		fmt.Println("")
+	}
+}
